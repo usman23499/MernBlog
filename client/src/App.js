9e680aff316e5461e16dc349411c8d1a69db3eff
Muhammad Usman Hussian <usman23499@gmail.com>
2021-09-16 16:51:02 +0500

    newnavbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import {BrowserRouter as Router, Route , Switch} from 'react-router-dom'
 import './main.scss';
 import Home from './components/home'
 import Registration from './components/auth/registration'
-import Login from './components/auth/login'
+import Login from './components/auth/login';
+import Navbar from './components/Navbar'
 function App() {
+
   return (
+
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/registration"  component={Registration} />
