b603f681d8238dabc806a9a1b4064f68e9a0ccc7
Muhammad Usman Hussian <usman23499@gmail.com>
2021-10-03 04:29:19 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,12 @@ module.exports=connect=async()=>{
    
     try {
         
-        const response=await mongoose.connect("mongodb+srv://mern:mern@cluster0.lk4lx.mongodb.net/blog?retryWrites=true&w=majority");
+        const response = await mongoose.connect(process.env.URL, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		
+		}); 
+        
         console.log("Connection Created");
     } catch (error) {
         console.log(error);
